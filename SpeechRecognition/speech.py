69fa29169ca363babfef130f503117bb37447929
jojilaan <uyilaan@gmail.com>
2017-06-03 12:39:16 +0200

    Changed speech.py to only work with the string 'pass the butter'

--- SpeechRecognition/speech.py
+++ SpeechRecognition/speech.py
@@ -8,15 +8,20 @@ import speech_recognition as sr
 #Loop the Speech Recognition 
 while (True):
 #First make the microphone listen for speechfragments
-	r = sr.Recognizer()
-	with sr.Microphone() as source:
-		print("What is my purpose?")
-		audio = r.listen(source)
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+                print("What is my purpose?")
+                audio = r.listen(source)
 
 #now send it to Google to do some magic with it, and return it as a string
-	try:
-		print("I think you said the following phrase:\n" + r.recognize_google(audio))
-	except sr.UnknownValueError:
-		print("I could not understand what you were saying...")
-	except sr.RequestError as e:        #if something went wrong...
-		print("The following error had occured from the Google Recognition Service: {0}".format(e))
+        try:
+                gString = r.recognize_google(audio)
+                #print(gString)                         #this line is for testing purposes only
+                if (gString == "pass the butter" or gString == "pasta butter"):
+                # if statement checks if the returned string from Google the same as "pass the
+                # butter" or "pasta butter"
+                        print("PASS THE BUTTER!!")
+        except sr.UnknownValueError:
+                print("I could not understand what you were saying...")
+        except sr.RequestError as e:        #if something goes wrong...
+                print("The following error had occured from the Google Recognition Service: {0}".format(e))
