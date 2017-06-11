748ea15a9fdf4ebe20430ff948f3cd37be3e181e
jojilaan <uyilaan@gmail.com>
2017-06-11 22:04:00 +0200

    Changed the Rover5 code

--- test_env/rover_5.py
+++ test_env/rover_5.py
@@ -2,22 +2,21 @@
 import RPi.GPIO as GPIO
 import time
 
-'''
-pin 16 voor voor-links
-pin 20 voor achter-links
+# pin 16 for PWM signal
+# pin 20 for Direction signal
 
-
-'''
 GPIO.setmode(GPIO.BCM)
+
 GPIO.setup(16, GPIO.OUT)
 GPIO.setup(20, GPIO.OUT)
+GPIO.output(20, GPIO.HIGH)      # or GPIO.LOW
 pwm = GPIO.PWM(16, 100)
-pwm.start(5)
+pwm.start(1)                    # Duty Cycle of 1
 
 for i in range(0, 100):
-    pwm.ChangeDutyCycle(100)
+    pwm.ChangeDutyCycle(100)    # Make the Rover Motors go full speed
     time.sleep(.1)
 #run for 10 sec.
 
-pwm.stop(pin)
+pwm.stop()
 pwm.cleanup()
