254bb3aee6f3dcf6b5cdf452a6dade08a3cd9409
jojilaan <uyilaan@gmail.com>
2017-06-11 22:11:55 +0200

    Updated rover-5.py

--- test_env/rover_5.py
+++ test_env/rover_5.py
@@ -2,21 +2,17 @@
 import RPi.GPIO as GPIO
 import time
 
-# pin 16 for PWM signal
-# pin 20 for Direction signal
-
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(16, GPIO.OUT)
-GPIO.setup(20, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)        # pin 16 for PWM
+GPIO.setup(20, GPIO.OUT)        # pin 20 for Direction
 GPIO.output(20, GPIO.HIGH)      # or GPIO.LOW
 pwm = GPIO.PWM(16, 100)
 pwm.start(1)                    # Duty Cycle of 1
 
-for i in range(0, 100):
+for i in range(0, 100):         # Loop for 10 seconds
     pwm.ChangeDutyCycle(100)    # Make the Rover Motors go full speed
     time.sleep(.1)
-#run for 10 sec.
 
 pwm.stop()
 pwm.cleanup()
